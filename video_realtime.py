d1d2a53d882d3f1860480beba23483fcf5219a0b
saurabh <saurabh.varsh@yahoo.com>
2018-01-25 17:40:02 +0100

    add my file for relatime video

---
+++ video_realtime.py
@@ -0,0 +1,40 @@
+import cv2
+from darkflow.net.build import TFNet
+import numpy as np
+import time
+
+option = {
+    'model': 'cfg/yolo.cfg',
+    'load': 'bin/yolo-new.weights',
+    'threshold': 0.30,
+    'gpu': 1.0
+}
+
+tfnet = TFNet(option)
+'''
+If result is not good reduce the frame rate of the video
+'''
+# Replace test_video.mp4 argument with 0 to test with camera stream
+capture = cv2.VideoCapture('test_video.mp4')
+colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
+
+while (capture.isOpened()):
+    stime = time.time()
+    ret, frame = capture.read()
+    results = tfnet.return_predict(frame)
+    if ret:
+        for color, result in zip(colors, results):
+            tl = (result['topleft']['x'], result['topleft']['y'])
+            br = (result['bottomright']['x'], result['bottomright']['y'])
+            label = result['label']
+            frame = cv2.rectangle(frame, tl, br, color, 4)
+            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX,
+                                1, (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
+        print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        capture.release()
+        cv2.destroyAllWindows()
+        break
